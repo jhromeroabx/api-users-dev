72c1284d05cf393105fcf26036705c74af20f470
Marcos Bueno <inboxg.marcos@gmail.com>
2024-06-03 13:18:43 -0300

    first commit en develop

--- src/main/java/com/demo/apiusers/ApiUsersApplication.java
+++ src/main/java/com/demo/apiusers/ApiUsersApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApiUsersApplication {
 
+
    public static void main(String[] args) {
       SpringApplication.run(ApiUsersApplication.class, args);
    }
