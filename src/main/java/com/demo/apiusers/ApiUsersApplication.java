8dde3bfcc72fc8f5106e8375c55d614a73675754
Marcos Bueno <inboxg.marcos@gmail.com>
2024-06-03 21:55:03 -0300

    Se agregó swagger a pom

--- src/main/java/com/demo/apiusers/ApiUsersApplication.java
+++ src/main/java/com/demo/apiusers/ApiUsersApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApiUsersApplication {
-
-
    public static void main(String[] args) {
       SpringApplication.run(ApiUsersApplication.class, args);
    }
