5bae12216fa87faa3b9334cdc18d5ade7bd3ac40
hernancaggiano <caggianohernan@gmail.com>
2024-06-05 13:06:05 -0300

    Correción nombre de tablas y columnas

--- src/main/java/com/demo/apiusers/model/RlRole.java
+++ src/main/java/com/demo/apiusers/model/RlRole.java
@@ -5,12 +5,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "rl_user_role")
 public class RlRole {
    @Id
    @GeneratedValue(strategy = GenerationType.IDENTITY)
+   @Column(name = "id_rl_user_role")
    private Long id;
-   @Column(nullable = false, unique = true)
+   @Column(nullable = false, unique = true, name = "descripcion")
    private String name;
 }
--- src/main/java/com/demo/apiusers/model/RlUser.java
+++ src/main/java/com/demo/apiusers/model/RlUser.java
@@ -7,17 +7,21 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "rl_user")
 public class RlUser {
    @Id
    @GeneratedValue(strategy = GenerationType.AUTO.IDENTITY)
+   @Column(name = "id_rl_user")
    private Long id;
-   @Column(nullable = false, unique = true)
+   @Column(nullable = false, unique = true, name = "username")
    private String username;
-   @Column(nullable = false)
+   @Column(nullable = false, name = "user_password")
    private String password;
    @ManyToOne
-   @JoinColumn(name = "role_id")
+   @JoinColumn(name = "id_system_user_role")
    private RlRole role;
+
 }
