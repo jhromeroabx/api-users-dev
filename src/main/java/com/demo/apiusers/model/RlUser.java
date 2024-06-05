2a70aaf433d3d47c3d584dddbe3d95ae0eaca95a
hernancaggiano <caggianohernan@gmail.com>
2024-06-05 17:17:35 -0300

    Correción de las clase

--- src/main/java/com/demo/apiusers/model/RlUser.java
+++ src/main/java/com/demo/apiusers/model/RlUser.java
@@ -8,18 +8,22 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "rl_user")
 public class RlUser {
    @Id
-   @GeneratedValue(strategy = GenerationType.AUTO.IDENTITY)
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
    @Column(name = "id_rl_user")
    private Long id;
    @Column(nullable = false, unique = true, name = "username")
