b46b2a021fd1b8868c1016b92198787146f67729
Jhosep Adbel Romero Loa <jhromero.abx@gmail.com>
2024-08-17 22:43:53 -0500

    add server

---
+++ src/main/java/com/demo/apiusers/config/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package com.demo.apiusers.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .servers(Arrays.asList(
+                        new Server().url("http://localhost:8080").description("API Users Dev Server"),
+                        new Server().url("https://api-users-dev.abexa.pe").description("API Users Deployed Server")
+                ));
+    }
+}
