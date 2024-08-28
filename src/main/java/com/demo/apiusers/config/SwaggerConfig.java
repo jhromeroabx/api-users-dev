2a103d3d7e5a938b206bae577e1736ef0d8f0961
Jhosep <jhromero.abx@gmail.com>
2024-08-28 11:00:44 -0500

    refix url AWS

--- src/main/java/com/demo/apiusers/config/SwaggerConfig.java
+++ src/main/java/com/demo/apiusers/config/SwaggerConfig.java
@@ -18,7 +18,7 @@ public class SwaggerConfig {
                         new Server().url("http://localhost:8080").description("API Users Dev Server"),
                         new Server().url("https://api-users-dev.abexa.pe").description("API Users Deployed Server Abx"),
                         new Server().url("https://api-users-dev.onrender.com").description("API Users Deployed Server Render"),
-                        new Server().url("https://tpcvmts9ri.us-east-2.awsapprunner.com").description("API Users Deployed Server AWS")
+                        new Server().url("https://ygp43pphan.us-east-2.awsapprunner.com").description("API Users Deployed Server AWS")
                 ));
     }
 }
